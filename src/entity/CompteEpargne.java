b45085e2aadeb177106e91a676bfff29549d1925
hamzalhadouchi <hamzabniss16@gmail.com>
2025-09-30 21:24:33 +0100

    Feat : Extend CompteEpargne class to include interest rate and override account type method https://hamzaalhadouchi-1735.atlassian.net/browse/TB-23

--- src/entity/CompteEpargne.java
+++ src/entity/CompteEpargne.java
@@ -1,4 +1,20 @@
 package entity;
 
-public class CompteEpargne {
+import dao.enums.TypeCompte;
+
+public final class CompteEpargne extends Compte {
+    private double tauxInteret;
+
+    public CompteEpargne(String id, String numero, double solde, String idClient, double tauxInteret, TypeCompte type) {
+        super(id, numero, solde, idClient, type);
+        this.tauxInteret = tauxInteret;
+    }
+
+    public double getTauxInteret() { return tauxInteret; }
+    public void setTauxInteret(double tauxInteret) {
+        this.tauxInteret = tauxInteret;
+    }
+
+    @Override
+    public TypeCompte getType() { return TypeCompte.EPARGNE; }
 }

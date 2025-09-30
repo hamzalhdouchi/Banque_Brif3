ece0774db1394dfc18e74d33563a107c42db1f07
hamzalhadouchi <hamzabniss16@gmail.com>
2025-09-30 21:55:26 +0100

    Feat : Implement ClientDAOImpl class with ClientDAO interface integration https://hamzaalhadouchi-1735.atlassian.net/browse/TB-32

--- src/dao/ClientDAOImpl.java
+++ src/dao/ClientDAOImpl.java
@@ -1,4 +1,14 @@
 package dao;
 
-public class ClientDAOImpl {
+import dao.Interfase.ClientDAO;
+import entity.Client;
+import entity.Compte;
+import entity.CompteCourant;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ClientDAOImpl implements ClientDAO {
 }

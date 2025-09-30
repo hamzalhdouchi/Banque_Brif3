b15c8e1b6a0486f4a03ef9250fd59122af3aaf41
hamzalhadouchi <hamzabniss16@gmail.com>
2025-09-30 21:31:07 +0100

    Feat : Add methods to Transaction interface for adding and retrieving transactions https://hamzaalhadouchi-1735.atlassian.net/browse/TB-34

--- src/dao/interfase/Transaction.java
+++ src/dao/interfase/Transaction.java
@@ -1,4 +1,9 @@
 package dao.interfase;
 
+import java.util.List;
+
 public interface Transaction {
+    boolean ajouter(entity.Transaction transaction);
+
+    List<entity.Transaction> trouverToutes();
 }

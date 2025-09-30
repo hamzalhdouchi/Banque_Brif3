722acdb5963256e51b6a4a5221a3ad0d9d1bf4c4
hamzalhadouchi <hamzabniss16@gmail.com>
2025-09-30 21:47:12 +0100

    Feat : Implement ajouter method in CompteDAOImpl for account insertion https://hamzaalhadouchi-1735.atlassian.net/browse/TB-33

--- src/dao/CompteDAOImpl.java
+++ src/dao/CompteDAOImpl.java
@@ -1,6 +1,12 @@
 package dao;
 
+import entity.Compte;
+import entity.CompteCourant;
+import entity.CompteEpargne;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class CompteDAOImpl {
 
@@ -8,5 +14,32 @@ public class CompteDAOImpl {
 
     public CompteDAOImpl(Connection connection) {
         this.connection = connection;
+    }
+
+    @Override
+    public boolean ajouter(Compte compte) {
+        String sql = "insert into compte(numero,solde,id_client,type_compte,decouvert_autorise,taux_interet) VALUES (?,?,?,?,?,?)";
+
+        try{
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, compte.getNumero());
+            ps.setDouble(2, compte.getSolde());
+            ps.setString(3,compte.getIdClient());
+            ps.setString(4,compte.getType().toString());
+            if (compte instanceof CompteCourant){
+                ps.setDouble(5,((CompteCourant) compte).getDecouvertAutorise());
+                ps.setDouble(6,0);
+            }else if (compte instanceof CompteEpargne){
+                ps.setDouble(5,0);
+                ps.setDouble(6,((CompteEpargne) compte).getTauxInteret());
+            }
+            ps.executeUpdate();
+            System.out.println("the nemuro de votre compte   : " +  compte.getNumero());
+            return true;
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+
+        }
+        return false;
     }
 }

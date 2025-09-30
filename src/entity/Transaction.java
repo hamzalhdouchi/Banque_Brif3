213ad7307cbef66d0577309f31cbcb1142995a58
hamzalhadouchi <hamzabniss16@gmail.com>
2025-09-30 21:25:24 +0100

    Feat : Enhance Transaction class with validation and default value for lieu https://hamzaalhadouchi-1735.atlassian.net/browse/TB-23

--- src/entity/Transaction.java
+++ src/entity/Transaction.java
@@ -1,4 +1,19 @@
 package entity;
 
-public class Transaction {
+import dao.enums.TypeTransaction;
+
+import java.time.LocalDateTime;
+
+public record Transaction(String id, LocalDateTime date, double montant, TypeTransaction type, String lieu, String idCompte) {
+
+
+    public Transaction {
+        if (montant <= 0) {
+            throw new IllegalArgumentException("Le montant doit être positif");
+        }
+        if (lieu == null || lieu.trim().isEmpty()) {
+            lieu = "Non spécifié";
+        }
+    }
+
 }

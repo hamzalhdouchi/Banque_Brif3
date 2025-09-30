ceeec54ae8f45dc5a7128e61381042fc06bc5b59
hamzalhadouchi <hamzabniss16@gmail.com>
2025-09-30 21:44:41 +0100

    Feat : Enhance CompteDAO interface with CRUD methods and balance update functionality https://hamzaalhadouchi-1735.atlassian.net/browse/TB-33

--- src/dao/Interfase/CompteDAO.java
+++ src/dao/Interfase/CompteDAO.java
@@ -1,4 +1,13 @@
 package dao.Interfase;
 
+import entity.Compte;
+import java.util.List;
+import java.util.Optional;
+
 public interface CompteDAO {
+    boolean ajouter(Compte compte);
+    void modifier(Compte compte);
+    boolean supprimer(String id);
+    public List<Compte>  trouverTous();
+    public boolean mettreAJourSolde(String idCompte, double nouveauSolde);
 }

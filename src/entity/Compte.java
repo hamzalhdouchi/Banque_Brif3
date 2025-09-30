bb350203e65abe014dafe93f5ec4de6d17f8e19d
hamzalhadouchi <hamzabniss16@gmail.com>
2025-09-30 21:23:15 +0100

    Feat : Implement abstract Compte class with properties and methods for account management https://hamzaalhadouchi-1735.atlassian.net/browse/TB-23

--- src/entity/Compte.java
+++ src/entity/Compte.java
@@ -1,4 +1,30 @@
 package entity;
 
-public class Compte {
+import dao.enums.TypeCompte;
+
+public sealed abstract class Compte permits CompteCourant, CompteEpargne {
+    private final String id;
+    private final String numero;
+    private double solde;
+    private final String idClient;
+    private final TypeCompte type;
+
+    public Compte(String id, String numero, double solde, String idClient, TypeCompte type) {
+        this.id = id;
+        this.numero = numero;
+        this.solde = solde;
+        this.idClient = idClient;
+        this.type = type;
+    }
+
+    // Getters
+    public String getId() { return id; }
+    public String getNumero() { return numero; }
+    public double getSolde() { return solde; }
+    public String getIdClient() { return idClient; }
+
+    // Setters
+    public void setSolde(double solde) { this.solde = solde; }
+
+    public  TypeCompte getType(){ return type; };
 }

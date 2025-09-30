e8305ce381c35c29457a2045df244b37fb8c2a7e
hamzalhadouchi <hamzabniss16@gmail.com>
2025-09-30 21:23:52 +0100

    Feat : Implement CompteCourant class with properties for authorized overdraft and account type https://hamzaalhadouchi-1735.atlassian.net/browse/TB-23

--- src/entity/CompteCourant.java
+++ src/entity/CompteCourant.java
@@ -1,4 +1,20 @@
 package entity;
 
-public class CompteCourant {
+import dao.enums.TypeCompte;
+
+public final class CompteCourant extends Compte {
+    private double decouvertAutorise;
+
+    public CompteCourant(String id, String numero, double solde, String idClient, double decouvertAutorise, TypeCompte type) {
+        super(id, numero, solde, idClient,type);
+        this.decouvertAutorise = decouvertAutorise;
+    }
+
+    public double getDecouvertAutorise() { return decouvertAutorise; }
+    public void setDecouvertAutorise(double decouvertAutorise) {
+        this.decouvertAutorise = decouvertAutorise;
+    }
+
+    @Override
+    public TypeCompte getType() { return TypeCompte.COURANT; }
 }

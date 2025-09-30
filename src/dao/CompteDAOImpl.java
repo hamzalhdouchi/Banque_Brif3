a752fe0b8a86e04b261e9eec9794896e755da20e
hamzalhadouchi <hamzabniss16@gmail.com>
2025-09-30 21:46:36 +0100

    Feat : Add constructor to CompteDAOImpl for dependency injection of Connection https://hamzaalhadouchi-1735.atlassian.net/browse/TB-33

--- src/dao/CompteDAOImpl.java
+++ src/dao/CompteDAOImpl.java
@@ -1,4 +1,12 @@
 package dao;
 
+import java.sql.Connection;
+
 public class CompteDAOImpl {
+
+    private static Connection connection;
+
+    public CompteDAOImpl(Connection connection) {
+        this.connection = connection;
+    }
 }

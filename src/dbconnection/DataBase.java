037b4bdb3048a45b02fa41cd36b305813be92d4b
hamzalhadouchi <hamzabniss16@gmail.com>
2025-09-30 21:12:42 +0100

    Feat : Implement singleton pattern and add database connection details in DataBase class https://hamzaalhadouchi-1735.atlassian.net/browse/TB-10

--- src/dbconnection/DataBase.java
+++ src/dbconnection/DataBase.java
@@ -3,4 +3,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class DataBase {
+    private static DataBase instance = null;
+    private static Connection connection;
+    public static final String URL = "jdbc:mysql://localhost:3306/gestion_bancaire?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "37533753";
 }

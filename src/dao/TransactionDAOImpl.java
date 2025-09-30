75570eadabcdf7e770463caf6680fe06c9c0d2bf
hamzalhadouchi <hamzabniss16@gmail.com>
2025-09-30 21:35:45 +0100

    Feat : Add method to TransactionDAOImpl for retrieving all transactions from the database https://hamzaalhadouchi-1735.atlassian.net/browse/TB-34

--- src/dao/TransactionDAOImpl.java
+++ src/dao/TransactionDAOImpl.java
@@ -41,5 +41,45 @@ public class TransactionDAOImpl implements TransactionDAO {
         return false;
     }
 
+    public boolean supprimer(Transaction transaction) {
+        String sql = "delete from transaction where id = ?";
+        try{
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, String.valueOf(transaction.id()));
+            ps.execute();
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Erreur Ajout de la transaction "+e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public List<Transaction> trouverToutes() {
+
+        String sql = "select * from transaction";
+        List<Transaction> transactions = new ArrayList<>();
+        try{
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                String id = rs.getString("id");
+                String date_transaction = rs.getString("date_transaction");
+                double montant = rs.getDouble("montant");
+                String type_transaction = rs.getString("type_transaction");
+                String id_compte = rs.getString("id_compte");
+                String lieu = rs.getString("lieu");
+                LocalDateTime date_compte = LocalDateTime.parse(date_transaction, formatter);
+                TypeTransaction type = TypeTransaction.valueOf(type_transaction);
+                Transaction transaction = new Transaction(id,date_compte,montant,type,lieu,id_compte);
+                transactions.add(transaction);
+            }
+            return transactions;
+        } catch (SQLException e) {
+            return  null;
+        }
+
+    }
 
 }

a6ef24055f16ec5d9d2a7b2412c9528449032552
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-01 22:50:04 +0100

    Feat : Add ClientService class for managing client-related operations https://hamzaalhadouchi-1735.atlassian.net/browse/TB-63

---
+++ src/service/ClientService.java
@@ -0,0 +1,14 @@
+package service;
+
+import dao.CompteDAOImpl;
+import dao.Interfase.ClientDAO;
+import dao.Interfase.CompteDAO;
+import entity.Client;
+import entity.Compte;
+import util.ValidationUtil;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ClientService {
+}

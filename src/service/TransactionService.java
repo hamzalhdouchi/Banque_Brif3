9114966458f37ae93eaafd70e1d032ab24b6f1a1
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-02 23:30:57 +0100

    Feat : Implement TransactionService class for transaction management https://hamzaalhadouchi-1735.atlassian.net/browse/TB-74

---
+++ src/service/TransactionService.java
@@ -0,0 +1,19 @@
+package service;
+
+import dao.Interfase.CompteDAO;
+import dao.Interfase.TransactionDAO;
+import enums.TypeTransaction;
+import entity.Compte;
+import entity.Transaction;
+import service.InterfaceService.TransactionServiceInterface;
+
+import java.time.LocalDateTime;
+import java.time.YearMonth;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class TransactionService implements TransactionServiceInterface {
+}

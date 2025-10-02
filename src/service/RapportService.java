f372bec40c283fa77d04e0fd0bdf2f2323b662df
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-02 23:21:47 +0100

    Feat : Implement RapportService class with interface and necessary imports
    https://hamzaalhadouchi-1735.atlassian.net/browse/TB-74

--- src/service/RapportService.java
+++ src/service/RapportService.java
@@ -1,4 +1,15 @@
 package service;
 
-public class RapportService {
+import enums.TypeTransaction;
+import entity.Client;
+import entity.Compte;
+import entity.Transaction;
+import service.InterfaceService.RapportServiceInterface;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class RapportService implements RapportServiceInterface {
 }

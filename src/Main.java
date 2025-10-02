f1cf9317d64e153d33f99ac60be19ce797f20f54
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-03 00:07:45 +0100

    Feat : Initialize main application with service and menu classes

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import dao.ClientDAOImpl;
+import dao.CompteDAOImpl;
+import dao.Interfase.ClientDAO;
+import dao.Interfase.CompteDAO;
+import dao.Interfase.TransactionDAO;
+import dbconnection.DataBase;
+import service.ClientService;
+import service.CompteService;
+import service.RapportService;
+import service.TransactionService;
+import ui.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.sql.Connection;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Connection c = DataBase.getInstance().getConnection();
+        ClientDAO clientDAO = new ClientDAOImpl(c);
+        CompteDAO compteDAO = new CompteDAOImpl(c);
+        TransactionDAO transactionDAO = new dao.TransactionDAOImpl(c);
+        ClientService clientService = new ClientService(clientDAO,compteDAO);
+        CompteService compteService = new CompteService(compteDAO);
+        TransactionService transactionService = new TransactionService(transactionDAO,compteDAO);
+        RapportService rapportService = new RapportService(compteService,clientService,transactionService);
+        MenuClient client = new MenuClient(clientService);
+        MenuTransaction transaction = new MenuTransaction(transactionService);
+        MenuCompte compte = new MenuCompte(compteService);
+        MenuRapport MenuRapport = new MenuRapport(rapportService);
+        MenuPrincipal menu = new MenuPrincipal(transaction,client,compte,MenuRapport);
+        menu.menuPrincipal();
+
+
     }
 }

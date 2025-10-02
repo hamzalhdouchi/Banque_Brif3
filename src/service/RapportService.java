79ffc30717804f9291a9af2f5d69b7d46741242b
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-02 23:22:27 +0100

    Feat : Add constructor to RapportService for dependency injection of services https://hamzaalhadouchi-1735.atlassian.net/browse/TB-74

--- src/service/RapportService.java
+++ src/service/RapportService.java
@@ -12,4 +12,13 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class RapportService implements RapportServiceInterface {
+
+    private final CompteService compteService;
+    private final ClientService clientService;
+    private final TransactionService transactionService;
+    public RapportService(CompteService compteService, ClientService clientService, TransactionService transactionService) {
+        this.compteService = compteService;
+        this.clientService = clientService;
+        this.transactionService = transactionService;
+    }
 }
